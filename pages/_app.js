21cb4052bca15337577766ca1ed131ca409e9cf2
anhtoan260497 <anhtoan260497@gmail.com>
2024-03-23 03:23:54 +0700

    add library

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,50 @@
 import Header from "@/components/Header"
-import { MoralisProvider } from "react-moralis"
+import Head from "next/head"
+import '@rainbow-me/rainbowkit/styles.css';
+
+import {
+    getDefaultConfig,
+    RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import {
+    mainnet,
+    polygon,
+    optimism,
+    arbitrum,
+    base,
+    sepolia
+} from 'wagmi/chains';
+import {
+    QueryClientProvider,
+    QueryClient,
+} from "@tanstack/react-query";
+
+const config = getDefaultConfig({
+    appName: 'My RainbowKit App',
+    projectId: 'YOUR_PROJECT_ID',
+    chains: [mainnet, polygon, optimism, arbitrum, base],
+    ssr: true, // If your dApp uses server side rendering (SSR)
+});
+
+const queryClient = new QueryClient();
+
 
 export default function App({ Component, pageProps }) {
     return (
-        <MoralisProvider initializeOnMount={false}>
-            <Header />
-            <Component {...pageProps} />
-        </MoralisProvider>
+        <>
+            <Head>
+                <title>NFT Marketplace</title>
+                <link rel="icon" href="https://styles.redditmedia.com/t5_9vkrm/styles/communityIcon_dy4kuclo24gb1.png"></link>
+            </Head>
+            <WagmiProvider config={config}>
+                <QueryClientProvider client={queryClient}>
+                    <RainbowKitProvider>
+                        <Header />
+                        <Component {...pageProps} />
+                    </RainbowKitProvider>
+                </QueryClientProvider>
+            </WagmiProvider>
+        </>
     )
 }
